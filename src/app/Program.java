a778315b553c6af82a5a8b8b3ea01313e89dcbea
Ruderson Florentino <rudersonvf@gmail.com>
2022-10-06 19:08:54 -0300

    implemented methods and main

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,10 +1,50 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.TaxPayer;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		List<TaxPayer> list = new ArrayList<>();
+		
+		System.out.print("Quantos contribuintes você vai digitar? ");
+		int n = scan.nextInt();
+		System.out.println();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.printf("Digite os dados do %do contribuinte:%n", i + 1 );
+			System.out.print("Renda anual com salário: ");
+			double salaryIncome = scan.nextDouble();
+			System.out.print("Renda anual com prestação de serviço: ");
+			double servicesIncome = scan.nextDouble();
+			System.out.print("Renda anual com ganho de capital: ");
+			double capitalIncome = scan.nextDouble();
+			System.out.print("Gastos médicos: ");
+			double healthSpending = scan.nextDouble();
+			System.out.print("Gastos educacionais: ");
+			double educationalSpending = scan.nextDouble();
+			System.out.println();
+			
+			TaxPayer taxPayer = new TaxPayer(salaryIncome, servicesIncome, capitalIncome, healthSpending, educationalSpending);
+			
+			list.add(taxPayer);
+			
+		}
+		
+		for(TaxPayer x : list) {
+			System.out.println(x);
+		}
+		
+		scan.close();
 	}
 
 }
--- src/entities/TaxPayer.java
+++ src/entities/TaxPayer.java
@@ -1,5 +1,98 @@
 package entities;
 
 public class TaxPayer {
+	
+	private Double salaryIncome;
+	private Double servicesIncome;
+	private Double capitalIncome;
+	private Double healthSpending;
+	private Double educationSpending;
+	
+	public TaxPayer() {
+	}
+	
+	public TaxPayer( Double salaryIncome, Double servicesIncome, Double capitalIncome, Double healthSpending, Double educationSpending) {
+		this.salaryIncome = salaryIncome;
+		this.servicesIncome = servicesIncome;
+		this.capitalIncome = capitalIncome;
+		this.healthSpending = healthSpending;
+		this.educationSpending = educationSpending;
+	}
 
+	//Gets Sets
+	
+	public Double getHealthSpending() {
+		return healthSpending;
+	}
+
+	public void setHealthSpending(Double healthSpending) {
+		this.healthSpending = healthSpending;
+	}
+
+	public Double getEducationSpending() {
+		return educationSpending;
+	}
+
+	public void setEducationSpending(Double educationSpending) {
+		this.educationSpending = educationSpending;
+	}
+
+	public Double getSalaryIncome() {
+		return salaryIncome;
+	}
+
+	public Double getServicesIncome() {
+		return servicesIncome;
+	}
+
+	public Double getCapitalIncome() {
+		return capitalIncome;
+	}
+	
+	//Methods
+	
+	public double salaryTax(double salaryIncome) {
+		if(salaryIncome / 12 < 3000.0) {
+			return 0.0;
+		}
+		else if(salaryIncome / 12  < 5000.0 ) {
+			return salaryIncome * 0.1;
+		}
+		else {
+			return salaryIncome * 0.2;
+		}
+	}
+	
+	public double serviceTax(double servicesIncome) {
+		return servicesIncome * 0.15;
+	}
+	
+	public double capitalTax(double capitalIncome) {
+		return capitalIncome * 0.2;
+	}
+	
+	public double grossTax(double salaryIncome, double servicesIncome, double capitalIncome) {
+		return salaryTax(salaryIncome) + serviceTax(servicesIncome) + capitalTax(capitalIncome);
+	}
+
+	public double taxRebate(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending, double educationSpending) {
+		if(grossTax(salaryIncome, servicesIncome, capitalIncome) * 0.3 > healthSpending + educationSpending) {
+			return healthSpending + educationSpending;
+		}
+		else {
+			return grossTax(salaryIncome, servicesIncome, capitalIncome) * 0.3;
+		}
+	}
+	
+	public double netTax(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending, double educationSpending) {
+		return grossTax(salaryIncome, servicesIncome, capitalIncome) - taxRebate(salaryIncome, servicesIncome, capitalIncome, healthSpending, educationSpending);
+	}
+	
+	public String toString() {
+		return String.format("%n")
+				+ String.format("Resumo do %do contribuinte:%n", 0 + 1)
+				+ String.format("Imposto bruto total: %.2f%n", grossTax(salaryIncome, servicesIncome, capitalIncome))
+				+ String.format("Abatimento: %.2f%n", taxRebate(salaryIncome, servicesIncome, capitalIncome, healthSpending, educationSpending))
+				+ String.format("Imposto devido: %.2f%n", netTax(salaryIncome, servicesIncome, capitalIncome, healthSpending, educationSpending));
+	}
 }
